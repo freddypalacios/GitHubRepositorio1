042700cb1f46bbab5f51308d1f2d8679edacbe56
Freddy Palacios <105250019+freddypalacios@users.noreply.github.com>
2025-04-22 09:16:50 -0600

    Update Program.cs
    Actualizada la población

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 //defincion e inicialización de los arreglos en paralelo y el diccionario
 String[] Departamento = { "Boaco", "Carazo", "Chinandega", "Chontales", "Costa Caribe Norte", "Costa Caribe Sur", "Estelf", "Granada", "Jinotega", "León", "Madriz", "Managua", "Masaya", "Matagalpa", "Nueva Segovia", "Rio San、Juan", "Rivas" };
-int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744, 1546939, 391903, 593503, 271581, 135446, 182645 };
+int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744,  1575819, 391903, 593503, 271581, 135446, 182645 };
 Dictionary<string, int> diccionario = Departamento
 .Zip(Poblacion, (k, v) => new { Clave = k, Valor = v })
 .ToDictionary(x => x.Clave, x => x.Valor);
